2d8c62a9c707625c538e6746a9aa210bb988dddf
Barry Zeeberg <barryz2013@gmail.com>
2024-04-30 22:00:03 +0000

    version 1.2.0

--- R/NoviceDeveloperResources.R
+++ R/NoviceDeveloperResources.R
@@ -15,32 +15,89 @@
 #' The user can "cat" the return value and copy and paste to facilitate doing
 #'  this manually.
 #'
-#' @return returns a character string that can be printed using "echo" and then
+#' @examples
+#' \dontrun{
+#' # you need to specify dir, packs that are on your own computer !!
+#' dir1<-"~/personal/hearts/hearts_card_game_bayesian_inference"
+#' dir2<-"packages/inference_packages/inference_packages/"
+#' packs<-c("cardUtils","clickableImageMap")
+#' l<-checkBuildInstallSourcePackage(sprintf("%s/%s",dir1,dir2),packs,packs,TRUE)
+#' }
+#'
+#' @return returns a list whose components are
+#' \itemize{
+#' \item
+#' character string that can be printed using "echo" and then
 #' copy and paste by the user to load or update the packages
+#' \item
+#' return value of dateTable()
+#' }
 #'
 #' @export
 checkBuildInstallSourcePackage<-
   function(dir,packs,packCheck,autoLibrary=FALSE) {
     l<-list()
+    dt<-list()
     returnStr<-""
+    dir<-path.expand(dir)
+    
+    lib<-getSysLib()
+    
     for(pack in packs) {
       p<-sprintf("%s/%s",dir,pack)
+      message(c("Processing: ",pack))
+      pattern<-sprintf("%s.+%s",pack,".tar.gz")
+      p.tar.gz<-list.files(dir,pattern=pattern)
+      if(length(p.tar.gz)>0)
+        dt<-dateTable(dt,sprintf("%s/%s",dir,p.tar.gz),"before")
       oldwd <- getwd()
       on.exit(setwd(oldwd))
       setwd(p)
-      
+
       # check() local source package
       # to save time, check() only those packs in packCheck
-      if(pack %in% packCheck)
-        devtools::check()
+      if(pack %in% packCheck) {
+        devtoolsCheckErrors<-devtools::check()
+        if(devtoolsCheckErrors$status!=0)
+          stop(c("devtoolsCheckErrors",pack,devtoolsCheckErrors$stderr,devtoolsCheckErrors$status,devtoolsCheckErrors$errors,devtoolsCheckErrors$warnings))
+      }
       
       # build local .tar.gz source file
       devtools::document(roclets = c('rd', 'collate', 'namespace', 'vignette'))
+
+      b<-"buildError"
       b<-devtools::build()
+      # return value b is "a string giving the location (including file name) of the built package"
+      # an extra "/" at the end of dir will cause a problem in (b %in% lf)
+      #if(grep("/$",dir)==1)
+      if(length(grep("/$",dir))>0)
+        dir<-substr(dir,1,nchar(dir)-1)
+      lf<-list.files(dir,pattern=sprintf("%s.+.tar.gz",pack),full.names=TRUE)
+      if(!(b %in% lf))
+        stop(c("buildError: ",pack))
+
+      fExistsAge(b)
+      
+      dt<-dateTable(dt,b,"after")
       
       # install package from local .tar.gz file
-      install.packages(b,repos=NULL,type='source')
+      f<-sprintf("%s/%s",lib,pack)
+      dt<-dateTable(dt,f,"before")
+      # not well documented anywhere, but I fortuitously noticed that verbose=TRUE generates a message with the word "succeeded"
+      # https://stackoverflow.com/questions/64797849/how-to-save-verbose-output
+      installReturnStr<-capture.output(install.packages(b,repos=NULL,type='source',lib=lib,verbose=TRUE),type="message")
+      installReturnCode<-strsplit(installReturnStr[2],"[[:space:]]+")[[1]][2]
+      print(c("installReturnCode:",pack,installReturnCode))
+      if(installReturnCode!="succeeded")
+        stop(c("installReturnCode!='succeeded'"),pack)
       
+      dt<-dateTable(dt,f,"after")
+      fExistsAge(f)
+      
+      
+      # https://stackoverflow.com/questions/46916283/how-to-reinstall-and-reload-a-local-r-package
+      # "If a package is already loaded library() doesn't load it again"
+      # "You have to unload the current version of the package for the update to take effect when you try to load it again."
       # https://stackoverflow.com/questions/64737686/why-library-or-require-should-not-be-used-in-a-r-package
       # instead returns a character string that can be printed using
       # cat [return value of checkBuildInstallSourcePackage()]
@@ -49,12 +106,14 @@ checkBuildInstallSourcePackage<-
       returnStr<-sprintf("%s\nlibrary(%s)",returnStr,pack)
       
       # workaround for not being able to directly include library() in package code
-      if(autoLibrary) {
-        eval(parse(text = sprintf("detach(\"package:%s\")",pack)))
-        eval(parse(text = sprintf("library(%s)",pack)))
-      }
+      if(autoLibrary)
+        dt<-zload(lib,pack,dt)
     }
-    return(returnStr)
+    
+    l$dt<-dt
+    l$returnStr<-returnStr
+    
+    return(l)
   }
 
 #' conflictOfInterest
@@ -121,6 +180,13 @@ conflictOfInterest<-
 #' 
 #' @param packs character vector of user-specified packages
 #' 
+#' @examples
+#' \dontrun{
+#' # you need to specify packs that are on your own computer !!
+#' pack<-c("retrieve","tcpflow")
+#' conflictOfInterestRestricted(pack)
+#' }
+#' 
 #' @return returns a subset of the return value of conflictOfInterest()
 #' 
 #' @export
@@ -160,4 +226,193 @@ conflictOfInterestRestricted<-
         return(NULL)
     }
     return(NULL)
-}
+  }
+
+#' getSysLib
+#' 
+#' @description For consistency, make sure that we are always using the designated library rather than a random library
+#' 
+#' @return returns a character string containing the path name for the designated library
+#' 
+#' @examples
+#' if(interactive()) {
+#'   sysLib<-getSysLib()
+#' }
+#' 
+#' @export
+#' 
+getSysLib<-
+  function() {
+    libs<-.libPaths()
+    nlib<-length(libs)
+    if(nlib==0)
+      stop("getSysLib: you have no libraries")
+    cat("Here are your libraries:\n\n")
+    print(libs,quote=FALSE)
+    while(TRUE) {
+      lib<-as.integer(readline(prompt="Enter the number of the library that you wish to designate: "))
+      if(lib>=1 & lib<=nlib) {
+        cat(sprintf("\nYou chose %s\n",libs[lib]))
+        cat("Remember that in case you need to access the packages in the designated library in the future\n")
+        return(libs[lib])
+      }
+    }
+  }
+#' fExistsAge
+#' 
+#' @description check for newly created file existence and age
+#' 
+#' @param f character string containing the path name of the file
+#' 
+#' @examples
+#' \dontrun{
+#' # you need to specify f that is on your own computer !!
+#' dir1<-"/Users/barryzeeberg/personal/hearts"
+#' dir2<-"hearts_card_game_bayesian_inference/packages"
+#' f<-"NoviceDeveloperResources_1.1.0.tar.gz"
+#' fExistsAge(sprintf("%s/%s/%s",dir1,dir2,f))
+#' }
+#' 
+#' @return returns no values,but has side effect of terminating if file is not valid
+#' 
+#' @export
+fExistsAge<-
+  function(f) {
+    delt<-10 # max acceptable age in seconds
+    units<-"secs"
+    if(is.null(f))
+      stop("fExistsAge: argument f is NULL")
+    
+    if(length(f)==0)
+      stop("fExistsAge: length of argument f is 0")
+    
+    if(!file.exists(f))
+      stop(c("fExistsAge file does not exist: ",f))
+    age<-as.numeric(difftime(Sys.time(),file.info(f)$ctime,units=units))
+    if(age>delt)
+      stop(c("fExistsAge file too old: ",f," ",age," ",units))
+  }
+
+#' dateTable
+#' 
+#' @description list of file dates
+#' 
+#' @param dt date table in list format
+#' @param f character string containing the full path name of the file
+#' @param when character string either "before" or "after"
+#' 
+#' @details allows the user to confirm that the .tar.gz and the library packages are not left-overs
+#' 
+#' @examples
+#' \dontrun{
+#' # you need to specify dir, f that is on your own computer !!
+#' dt<-list()
+#' dir<-"~/personal/hearts/hearts_card_game_bayesian_inference/packages"
+#' f<-sprintf("%s/%s",dir,"NoviceDeveloperResources_1.1.0.tar.gz")
+#' when<-"before"
+#' dateTable(dt,f,when)
+#' }
+#' 
+#' @return updated version of dt
+#' 
+#' @export
+dateTable<-
+  function(dt,f,when) {
+    if(is.null(f))
+      stop("dateTable: argument f is NULL")
+    
+    if(length(f)==0)
+      stop("dateTable: length of argument f is 0")
+    
+   if(!file.exists(f)) {
+      dt[[f]]["now"]<-system("date",intern=TRUE)
+      dt[[f]]["error"]<-"file does not exist"
+      return(dt)
+    }
+    sys<-sprintf("ls -ld %s",f)
+    x<-system(sys,intern=TRUE)
+    ss<-strsplit(x,"[[:space:]]+")
+    dt[[f]]["now"]<-system("date",intern=TRUE)
+    dt[[f]][when]<-sprintf("%s %s %s",ss[[1]][6],ss[[1]][7],ss[[1]][8])
+    
+    return(dt)
+  }
+
+#' zload
+#' @description detach old version of package from search path, load new version, and validate
+#'
+#' @param lib character string containing the name of the user-designated library
+#' @param pack character string containing the name of a package
+#' @param dt date table in list format
+#'
+#' @examples
+#' \dontrun{
+#' # you need to specify packs that are on your own computer !!
+#' lib<-getSysLib()
+#' dt<-list()
+#' zload(lib,"NoviceDeveloperResources",dt)
+#' }
+#'
+#' @return returns updated version of date table
+#'
+#' @export
+zload<-
+  function(lib,pack,dt) {
+    # if package with the same name is already in the search path, detach it
+    if(inSearchPath(pack)) {
+      # version that is permitted in CRAN package)))
+      eval(parse(text = sprintf("detach(\"package:%s\", force=TRUE)",pack)))
+      if(inSearchPath(pack))
+        stop(c("zload detach() failed: ",pack))
+    }
+    # package is now guaranteed to be detached
+    # so we know that we are adding the intended version to the search path
+    # version that is permitted in CRAN package
+    eval(parse(text = sprintf("library(%s,lib.loc='%s', verbose=TRUE)",pack,lib)))
+    
+    # confirm that the package was successfully added to the search path
+    if(!inSearchPath(pack))
+      stop(c("zload library() failed: ",pack))
+    # double check to provide tangible proof that this package was
+    # newly added to the search path
+    # and that it came from the intended library
+    return(dateTable(dt,sprintf("%s/%s",lib,pack),"attach"))
+  }
+
+#' inSearchPack
+#' @description is the package listed in the search path
+#' 
+#' @param pack list of character strings containing the name of a package
+#' 
+#' @examples
+#' \dontrun{
+#' # you need to specify packs that are on your own computer !!
+#' pack<-c("retrieve","tcpflow")
+#' inSearchPath(pack)
+#' }
+#' 
+#' @return returns list of Booleans TRUE if the package is listed in the search path
+#' 
+#' @export
+inSearchPath<-
+  function(pack) {
+    # is pack in the search path?
+    return(formatSearchPack(pack) %in% search())
+  }
+
+#' formatSearchPack
+#' @description given a package name, reformat it as listed in the search path
+#' 
+#' @param pack list of character strings containing the names of a package
+#' 
+#' @examples
+#' pack<-c("retrieve","tcpflow")
+#' formatSearchPack(pack)
+#' 
+#' @return returns list of package names formatted it as listed in the search path
+#' 
+#' @export
+formatSearchPack<-
+  function(pack) {
+    return(sprintf("%s:%s","package",pack))
+  }
